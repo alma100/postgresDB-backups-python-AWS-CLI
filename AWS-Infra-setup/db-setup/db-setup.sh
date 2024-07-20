52d832dcb99abc1b34bad76f3a99998f0bcaf716
abaaba2 <grinacza@gmail.com>
2024-07-20 10:48:52 +0200

    modified db-setup

--- AWS-Infra-setup/db-setup/db-setup.sh
+++ AWS-Infra-setup/db-setup/db-setup.sh
@@ -2,14 +2,6 @@
 
 sudo yum update -y
 sudo yum upgrade -y
-sudo yum remove docker \
-  docker-client \
-  docker-client-latest \
-  docker-common \
-  docker-latest \
-  docker-latest-logrotate \
-  docker-logrotate \
-  docker-engine
 sudo yum install -y yum-utils \
   device-mapper-persistent-data \
   lvm2
